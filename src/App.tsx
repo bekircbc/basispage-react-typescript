d0dddc79555a1f5ef822e141b76193b635380df0
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-30 11:16:28 +0200

    added interface to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import { useState } from "react";
-import "./App.scss";
+import { useState } from 'react';
+import './App.scss';
+
+interface IPerson {
+	firstName: string;
+	lastName: string;
+	age: number;
+	isMember: boolean;
+	scores: number[];
+	accessGroups: string[];
+}
+
+const person: IPerson = {
+	firstName: 'Georg',
+	lastName: 'Deckner',
+	age: 34,
+	isMember: true,
+	scores: [38],
+	accessGroups: ['admins'],
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-  setCount(23);
-  return <div className="App">Hello </div>;
+	const [count, setCount] = useState(0);
+	setCount(23);
+	return (
+		<div className="App">
+			<p>
+				The person {person.firstName} {person.lastName} is {person.age}{' '}
+				years old. Their scoes are:{person.scores.join(',')}
+			</p>
+		</div>
+	);
 }
 
 export default App;
