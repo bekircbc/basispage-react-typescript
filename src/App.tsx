de30d474dbe44a2c7b0085a0f1ad91b3874023c6
Bekir - CDI <b.burakcebecii@gmail.com>
2022-05-30 11:05:44 +0200

    added prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,9 @@
-import "./App.css";
+import { useState } from "react";
+import "./App.scss";
 
 function App() {
+  const [count, setCount] = useState(0);
+  setCount(23);
   return <div className="App">Hello </div>;
 }
 
